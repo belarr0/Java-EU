a003fa70211afb7d860a0a9b807d97efa3485ccc
Bohdan Demych <belarr00ss@gmail.com>
2022-03-25 10:55:00 +0200

    add task 'emoji'

---
+++ emoji/src/Start.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main (String[] args){
+        String q = "\uD83D\uDE00";
+        String w = "\uD83D\uDE02";
+        String e = "\uD83D\uDE0D";
+        String r = "\uD83D\uDC7D";
+
+        System.out.print(q + " " + w + " " + e + " " + r);
+
+        //я просто копіював смайлики з 16 utf і вставлялв в код, вони самі перетворювалися в потрібний юнікод
+    }
+}
