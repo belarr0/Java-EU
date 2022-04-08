38f45f69098360e1c48ecbff848eecf9a1642e9e
Bohdan Demych <belarr00ss@gmail.com>
2022-04-08 13:37:48 +0300

    add task 3-5

--- while/src/Start.java
+++ while/src/Start.java
@@ -1,3 +1,4 @@
+import javax.security.sasl.SaslServer;
 import java.util.Scanner;
 
 public class Start {
@@ -24,7 +25,7 @@ public class Start {
             b++;
         }
         System.out.println(b);
-         */
+
 
         int n, a = 2;      //3
 
@@ -35,5 +36,32 @@ public class Start {
             a = a + a;
             System.out.println(a);
         }
+
+
+        int n, i = 1;       //4
+
+        System.out.print("Введіть число: ");
+        n = in.nextInt();
+
+        while (i < n) {
+            i = i * 2;
+        }
+        if (i == n) {
+            System.out.print("YES");
+        }else {
+            System.out.print("NO");
+        }
+         */
+
+        int n, k = 0, q = 1;       //5
+
+        System.out.print("Введіть число: ");
+        n = in.nextInt();
+
+        while (q < n) {
+            q = q * 2;
+            k++;
+        }
+        System.out.println("Відповідь: " + k);
     }
 }
