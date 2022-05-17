3ed3a1a4d9bef6f170eaff9a4cbc61f0b788b998
belarr0 <belarr00ss@gmail.com>
2022-05-17 13:45:48 +0300

    add new task

--- array/src/Start.java
+++ array/src/Start.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 public class Start {
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
+        /*
+        int[] array  = new int[] {3, 11, 43, 15, 16};       //1
 
-        int n;
+        for(int i = 0; i < 5; i++){
+            if(i % 2 == 0){
+                System.out.print(array[i] + " ");
+            }
+        }
 
-        n = in.nextInt();
-        if (n > 1 && n < 100){
-            
+
+        int[] arr = new int[] {1, 2, 3, 4, 5};      //2
+
+        for(int i = 0; i < 5; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+         */
+
+        int[] arr = new int[]{1, -3, 6, 2, -9};
+
+        for (int i = 0; i < 5; i++) {
+            if (arr[i] > 0){
+                System.out.print(arr[i] + " ");
+            }
         }
     }
 }
